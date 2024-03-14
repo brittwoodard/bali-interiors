4ab499c784176a7eb276719f610679b48fbcda59
brittwoodard <96916819+brittwoodard@users.noreply.github.com>
2024-03-14 09:54:38 +0100

    website/blog setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './_includes/**/*.{html,js,md}',
+      './_layouts/**/*.{html,js,md}',
+      './_*/*.{html,js,md}',
+      './*.{html,js,md}'
+    ],
+    theme: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+        serif: ['Italiana', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '5p': '5%',
+          '10p': '10%',
+          '15p': '15%',
+        },
+        aspectRatio: {
+          'landscape': '2 / 1 ',
+          'portrait': '3 / 4',
+          'portrait-tall': '3 / 5',
+        },
+      },
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
